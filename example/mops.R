c0c9b5a9837fe507a3e9d690aa843427c628b267
Mansun Kuo <coccoleagcy@gmail.com>
2016-01-08 07:03:17 +0800

    add key person

--- example/mops.R
+++ example/mops.R
@@ -17,12 +17,13 @@ knitr::opts_knit$set(root.dir = '..')
 library(httr)
 library(rvest)
 library(DT)
+library(data.table)
 
 #' ## Connector
 # Use character body
 url = "http://mops.twse.com.tw/mops/web/ajax_t51sb01"
 res = POST(url, body = "encodeURIComponent=1&step=1&firstin=1&TYPEK=sii&code=")
-doc_str = content(res, "text", encoding = "utf8")
+doc_str = content(res, as = "text", encoding = "utf8")
 
 # Use named list body
 body = list(encodeURIComponent="1",
@@ -31,21 +32,58 @@ body = list(encodeURIComponent="1",
             TYPEK="sii",
             code="")
 res2 = POST(url, body = body, encode = "form")
-doc_str2 = content(res, "text", encoding = "utf8")
-identical(doc_str, doc_str2)
-write(doc_str, file = "example/mops.html")
+doc_str2 = content(res2, "text", encoding = "utf8")
 
 #' ## Parser
+
 if (.Platform$OS.type == "windows"){
     Sys.setlocale(category='LC_ALL', locale='C')
-    data_table = doc_str %>% read_html(encoding = "big-5") %>% html_nodes(xpath = "//table[2]") %>% html_table(header=TRUE)
+    dt = doc_str %>% 
+        read_html(encoding = "big-5") %>% 
+        html_nodes(xpath = "//table[2]") %>% 
+        html_table(header=TRUE)
     Sys.setlocale(category='LC_ALL', locale='cht')
-    data_table = apply(data_table[[1]],2,function(x) iconv(x,from = "utf8"))
-    colnames(data_table) = iconv(colnames(data_table), from = "utf8")
+    dt = apply(dt[[1]],2,function(x) iconv(x,from = "utf8"))
+    colnames(dt) = iconv(colnames(dt), from = "utf8")
 } else{
-    data_table = doc_str %>% read_html(encoding = "big-5") %>% html_nodes(xpath = "//table[2]") %>% html_table(header=TRUE)
-    data_table = data_table[[1]]
+    dt = doc_str %>% 
+        read_html(encoding = "big-5") %>% 
+        html_nodes(xpath = "//table[2]") %>% 
+        html_table(header=TRUE)
+    dt = dt[[1]]
 }
 
+
 #' ## Result
-datatable(data_table)
+datatable(dt)
+
+
+#' ## Check if identical
+
+parse_table = function(doc_str, encoding = "big-5", xpath = "//table[2]") {
+    dt = doc_str %>% 
+        read_html(encoding = encoding) %>% 
+        html_nodes(xpath = xpath) %>% 
+        html_table(header=TRUE)
+    dt = dt[[1]]
+    return(dt)
+}
+
+if (.Platform$OS.type == "windows"){
+    Sys.setlocale(category='LC_ALL', locale='C')
+    print(identical(parse_table(doc_str), parse_table(doc_str2)))
+    Sys.setlocale(category='LC_ALL', locale='cht')
+} else{
+    identical(parse_table(doc_str), parse_table(doc_str2))
+}
+
+
+#' ## Who is the key person
+
+dt = data.table(dt)
+positions = c("董事長", "總經理", "發言人")
+heads = melt(dt, id.var = "公司名稱", measure.vars = positions,
+             variable.name = "position", value.name = "person")
+heads_count = heads[, .(count = .N), by = "person"][count > 1, ][order(-count), ]
+heads_count = suppressWarnings(heads_count[!person %in% positions, ])
+datatable(heads_count)
