a21fdc7cdb7575e4c589b477e40342506be4b07c
Mansun Kuo <coccoleagcy@gmail.com>
2016-01-09 08:01:38 +0800

    modify slides

--- README.md
+++ README.md
@@ -3,10 +3,31 @@
 A tutorial of Crawler in R.
 
 - [Slides](http://mansunkuo.github.io/RCrawler101-201512-2/)
-
 - [The entire series of course](https://github.com/datasci-info/RCrawler101-201512)
 
 
 ## Environment
 
 [R Installation Guide](http://mansunkuo.github.io/RCrawler101-201512-2/install_R.html)
+
+
+## How to Use This Slides
+
+On Laptop:
+
+- 'Cursor right' or 'Page Down': To go to next page or reveal next item
+- 'Cursor left' or 'Page Up': To do the reverse
+- 'C':  Show table of contents
+- 'F':  Toggles the display of the footer
+- 'A':  Toggles display of current vs all slides (useful for printing handouts)
+- 'S':  Make fonts smaller
+- 'B':  Make fonts large
+
+On mobile devides:
+
+- Swipe left to go to next page or reveal next item
+- Swipe right to do the reverse
+- Swipe with 2 fingers to stroll
+- Zoom in/out with standard touch gesture
+- Swipe up or down to show/hide the table of contents
+    
--- example/yahoo_stock.R
+++ example/yahoo_stock.R
@@ -1,5 +1,5 @@
 #' ---
-#' title: "Seven Eleven"
+#' title: "Yahoo Stock"
 #' author: "Agilelearning, Mansun Kuo"
 #' date: "`r Sys.Date()`"
 #' output: 
@@ -19,24 +19,62 @@ library(rvest)
 library(DT)
 
 
-## Connector
+#' ## Connector
 Target_URL = "http://tw.stock.yahoo.com/d/s/major_2451.html"
-res <- GET(Target_URL)
-doc_str <- content(res, type = "text", encoding = "big5")
+res = GET(Target_URL)
+
+#' ## Parser Solution 1
+
+# read as raw
+doc_str = content(res, type = "raw")
 
 ## Parser
 if (.Platform$OS.type == "windows"){
     Sys.setlocale(category='LC_ALL', locale='C')
-    df <- doc_str %>% 
-        read_html(encoding = "big-5") %>% 
+    dat = doc_str %>% 
+        read_html() %>% 
         html_nodes(xpath = "//table[1]//table[2]") %>% 
         html_table(header=TRUE)
     Sys.setlocale(category='LC_ALL', locale='cht')
-    df <- apply(df[[1]], 2, function(x) {iconv(x, from = "utf8")})
-    colnames(df) <- iconv(colnames(df), from = "utf8")
+    dat = dat[[1]]
+    dat = apply(dat, 2, function(x) {iconv(x, from = "utf8")})
+    colnames(dat) = iconv(colnames(dat), from = "utf8")
 }  else{
-    df <- doc_str %>% read_html(encoding = "big-5") %>% html_nodes(xpath = "//table[1]//table[2]") %>% html_table(header=TRUE)
-    df <- df[[1]]
+    dat = doc_str %>% 
+        read_html() %>% 
+        html_nodes(xpath = "//table[1]//table[2]") %>% 
+        html_table(header=TRUE)
+    dat = dat[[1]]
 }
 
-datatable(df)
+
+#' ## Parser Solution 2
+
+# check content-type
+headers(res)$`content-type`
+
+# read as Big5
+doc_str2 = content(res, type = "text", encoding = "Big5")
+
+## Parser
+if (.Platform$OS.type == "windows"){
+    Sys.setlocale(category='LC_ALL', locale='C')
+    dat2 = doc_str2 %>% 
+        read_html(encoding = "Big5") %>% 
+        html_nodes(xpath = "//table[1]//table[2]") %>% 
+        html_table(header=TRUE)
+    Sys.setlocale(category='LC_ALL', locale='cht')
+    dat2 = dat2[[1]]
+    dat2 = apply(dat2, 2, function(x) {iconv(x, from = "utf8")})
+    colnames(dat2) = iconv(colnames(dat2), from = "utf8")
+}  else{
+    dat2 = doc_str %>% 
+        read_html(encoding = "big-5") %>%
+        html_nodes(xpath = "//table[1]//table[2]") %>% 
+        html_table(header=TRUE)
+    dat2 = dat2[[1]]
+}
+
+identical(dat, dat2)
+datatable(dat)
+
